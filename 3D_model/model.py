aa63e16d9933daf5befef1731be6aad01ea5f33e
b-s-code <89502375+b-s-code@users.noreply.github.com>
2024-03-20 16:48:59 +0800

    encapsulate switchplate, signpost future work

--- 3D_model/model.py
+++ 3D_model/model.py
@@ -4,13 +4,14 @@ def emptysolid():
     return cube(0, 0, 0)
 
 class keydata:
-    """ 
-        Stores data related to a 1U keycap and switch.
+    """ Stores data related to a 1U keycap and switch.
         Sources: 
           - https://youtu.be/7azQkSu0m_U?t=71
           - https://www.cherry-world.com/cherry-mx/developer
     """
-    cap_width = 18 # mm.
+    cap_width = 18 # mm.  Switch manufacturer data states 18.5mm.
+                   # We'll use the number obtained from someone 
+                   # who actually prints switch plates instead.
     cap_space_width = 19.05 # mm.
     switch_hole_width = 14 # mm.
     switch_hole_separation = 5.05 # mm, apart.
@@ -36,8 +37,7 @@ class switch_hole_column:
         self.num_keys = num_keys 
 
     def subtrahend(self):
-        """ 
-            Returns a sum of rectangular prisms.
+        """ Returns a sum of rectangular prisms.
             This prism sum can be taken away from a switch plate to
             cut the holes needed for a key column's keyswitch holes.
         """
@@ -74,8 +74,7 @@ class switch_hole_thumb_cluster:
         self.num_keys = 2 
 
     def subtrahend(self):
-        """ 
-            Returns a sum of rectangular prisms.
+        """ Returns a sum of rectangular prisms.
             This prism sum can be taken away from a switch plate to
             cut the holes needed for the thumb cluster's keyswitch holes.
         """
@@ -92,22 +91,40 @@ class switch_hole_thumb_cluster:
             prism_sum += prism
         return prism_sum
 
-# Make uncut switch plate.
-switch_plate = cube(500, 500, 5)
+def make_switch_plate():
+    """ Returns a solidpython object representing the switch plate
+        (left hand side only), with holes cut for all key switches.
+    """
+    # Make uncut switch plate.
+    # Initial position and dimensions are arbitrarily large.
+    # Just need a big enough canvas to work with while arranging
+    # switch holes.
+    # TODO : tune the size, shape, position of the minuend of the
+    # switch plate.
+    switch_plate = cube(500, 500, 5).translate(-50, -50, 0)
+    
+    # Group everything we're going to cut out of the switch plate together.
+    subtrahends = []
+    
+    # Cut holes out of switch plate for columns of 1U keys.
+    z_offset = -0.5 # Prevent z-fighting.
+    subtrahends += [
+        switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, 0, z_offset),
+        switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, keydata.cap_space_width, z_offset),
+        switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, 2 * keydata.cap_space_width, z_offset),
+        switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, 3 * keydata.cap_space_width, z_offset)]
+    
+    # Intentionally one less 1U key in innermost column, to make room for thumb keys.
+    subtrahends.append(switch_hole_column(num_keys=3, cut_depth=6).subtrahend().translate(0, 4 * keydata.cap_space_width, z_offset))
+    
+    # Cut holes out of switch plate for 2X 2U thumb keys.
+    z_offset = -0.5 # Prevent z-fighting.
+    # TODO : position properly.  Somehow account for 2U keycap size too.
+    switch_plate -= switch_hole_thumb_cluster(cut_depth=7).subtrahend().translate(0, 0, z_offset).rotate(0,0,-45).translate(100,100,z_offset)
+    
+    for cutout in subtrahends:
+        switch_plate -= cutout
+    return switch_plate
 
-# Cut holes out of switch plate for columns of 1U keys.
-z_offset = -0.5 # Prevent z-fighting.
-switch_plate -= switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, 0, z_offset)
-switch_plate -= switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, keydata.cap_space_width, z_offset)
-switch_plate -= switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, 2 * keydata.cap_space_width, z_offset)
-switch_plate -= switch_hole_column(num_keys=4, cut_depth=6).subtrahend().translate(0, 3 * keydata.cap_space_width, z_offset)
-# Intentionally one less 1U key in innermost column, to make room for thumb keys.
-switch_plate -= switch_hole_column(num_keys=3, cut_depth=6).subtrahend().translate(0, 4 * keydata.cap_space_width, z_offset)
-
-# Cut holes out of switch plate for 2X 2U thumb keys.
-z_offset = -0.5 # Prevent z-fighting.
-# TODO : position properly.  Somehow account for 2U keycap size too.
-switch_plate -= switch_hole_thumb_cluster(cut_depth=7).subtrahend().translate(0, 0, z_offset).rotate(0,0,-45).translate(100,100,z_offset)
-
-model = switch_plate 
+model = make_switch_plate()
 model.save_as_scad()
--- 3D_model/model.scad
+++ 3D_model/model.scad
@@ -1,5 +1,22 @@
 difference() {
-	cube(size = [500, 500, 5]);
+	translate(v = [-50, -50, 0]) {
+		cube(size = [500, 500, 5]);
+	}
+	translate(v = [100, 100, -0.5]) {
+		rotate(a = [0, 0, -45]) {
+			translate(v = [0, 0, -0.5]) {
+				union() {
+					cube(size = [0, 0, 0]);
+					translate(v = [0, 0.0, 0]) {
+						cube(size = [14, 14, 7]);
+					}
+					translate(v = [0, 19.05, 0]) {
+						cube(size = [14, 14, 7]);
+					}
+				}
+			}
+		}
+	}
 	translate(v = [0, 0, -0.5]) {
 		union() {
 			cube(size = [0, 0, 0]);
@@ -79,21 +96,6 @@ difference() {
 			}
 			translate(v = [38.1, 0, 0]) {
 				cube(size = [14, 14, 6]);
-			}
-		}
-	}
-	translate(v = [100, 100, -0.5]) {
-		rotate(a = [0, 0, -45]) {
-			translate(v = [0, 0, -0.5]) {
-				union() {
-					cube(size = [0, 0, 0]);
-					translate(v = [0, 0.0, 0]) {
-						cube(size = [14, 14, 7]);
-					}
-					translate(v = [0, 19.05, 0]) {
-						cube(size = [14, 14, 7]);
-					}
-				}
 			}
 		}
 	}
