cc0e5fa4ffbf648c6943ac84f856f1ae98342011
b-s-code <89502375+b-s-code@users.noreply.github.com>
2024-07-01 22:17:52 +0800

    function docstring

--- 3D_model/util_fns.py
+++ 3D_model/util_fns.py
@@ -2,6 +2,7 @@ from solid2.core.builtins.openscad_primitives import _OpenSCADObject
 from solid2 import cube
 
 def combine(l: list[_OpenSCADObject]) -> _OpenSCADObject:
+    """ Returns the union of all objects in the input list. """
     accumulator: _OpenSCADObject = cube(0, 0, 0)
     for obj in l:
         accumulator += obj
